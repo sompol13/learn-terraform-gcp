7dd454c18f2432f350461bb5788180fcf832f5df
Sompol Techawattanalertkij <sompol13.th@gmail.com>
2022-01-16 19:56:45 +0700

    - Provision a Debian VM Instance with the VPN Network. - Adding network tags `dev` and `web` to the VM Instance.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ## Terraform - Build Infrastructure GCP Example
-The sample configuration provisions a network and a Linux virtual machine.
+- The sample configuration provisions a VPN Network.
+
+## Terraform - Change Infrastructure
+- Provision a Debian VM Instance with the VPN Network.
+- Adding network tags `dev` and `web` to the VM Instance.
--- main.tf
+++ main.tf
@@ -31,9 +31,36 @@ provider "google" {
  * Use resource blocks to define components of your infrastructure.
  * A resource might be a physical component such as a server, 
  * or it can be a logical resource such as a Heroku application.
- */
+ *
+ * NOTE: The order in which resources are defined in your configuration files 
+ * does not affect how Terraform provisions your resources.
+ */ 
+
+// Provision a VPN Network.
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+// Provision a Debian VM Instance.
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    // This establishes a dependency between the two resources.
+    network = google_compute_network.vpc_network.name
+    // The presence of the access_config block, even without any arguments, 
+    // gives the VM an external IP address, making it accessible over the internet.
+    access_config {
+    }
+  }
 }
 
 # terraform init - Initialize the directory
